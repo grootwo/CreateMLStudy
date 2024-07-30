76021fa1b04e11548d66fde8956c2b659595c984
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-30 09:45:32 +0900

    Feat: create BottleClassifier

---
+++ BottleClassifier/BottleClassifier/BottleClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottleClassifierApp.swift
+//  BottleClassifier
+//
+//  Created by Groo on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BottleClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
