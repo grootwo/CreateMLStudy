70a690122a1a2fe92150c6fb27787ff0ec589d82
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-30 11:18:07 +0900

    Feat: set PhotosPicker

--- BottleClassifier/BottleClassifier/ContentView.swift
+++ BottleClassifier/BottleClassifier/ContentView.swift
@@ -6,14 +6,22 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State private var pickerItem: PhotosPickerItem?
+    @State private var selectedImage: Image?
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            PhotosPicker("Tap to import a photo", selection: $pickerItem)
+                .onChange(of: pickerItem) {
+                    Task {
+                        selectedImage = try await pickerItem?.loadTransferable(type: Image.self)
+                    }
+                }
+            selectedImage?
+                .resizable()
+                .scaledToFit()
         }
         .padding()
     }
