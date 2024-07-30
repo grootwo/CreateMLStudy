1a009501beee41c40dbf9ad686ecbe38d4a6f9cd
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-07-30 12:25:42 +0900

    Feat: set func recognizeBottle with classifier but runtime error with CVPixelBuffer

--- BottleClassifier/BottleClassifier/ContentView.swift
+++ BottleClassifier/BottleClassifier/ContentView.swift
@@ -7,10 +7,18 @@
 
 import SwiftUI
 import PhotosUI
+import CoreML
 
 struct ContentView: View {
     @State private var pickerItem: PhotosPickerItem?
     @State private var selectedImage: Image?
+    @State private var answer: Set<String>?
+    var answerString: [String] {
+        if let answer {
+            return [String](answer)
+        }
+        return ["Unkown"]
+    }
     var body: some View {
         VStack {
             PhotosPicker("Tap to import a photo", selection: $pickerItem)
@@ -22,8 +30,26 @@ struct ContentView: View {
             selectedImage?
                 .resizable()
                 .scaledToFit()
+            if let answer {
+                Text(answerString.joined(separator: " / "))
+            }
+            Spacer()
+            Button("Bottle or not") {
+                recognizeBottle()
+            }
+            .disabled(selectedImage == nil)
         }
         .padding()
+    }
+    func recognizeBottle() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try BottleClassifier1(configuration: config)
+            let prediction = try model.prediction(image: selectedImage as! CVPixelBuffer)
+            answer = prediction.featureNames
+        } catch {
+            print("error")
+        }
     }
 }
 
