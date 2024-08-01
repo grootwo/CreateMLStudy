64a511e6ee9a7a3f558a91f4347ed469e628f7e7
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-08-01 11:33:21 +0900

    Feat: set recognizeBottle

--- BottleClassifier/BottleClassifier/ShootPhotoView.swift
+++ BottleClassifier/BottleClassifier/ShootPhotoView.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ShootPhotoView: View {
     @State private var isShowPhotoLibrary = true
     @State private var image = UIImage()
+    @State private var probability: [String: Double]?
+    @State private var target = ""
     var body: some View {
         VStack {
             Image(uiImage: image)
@@ -17,6 +20,18 @@ struct ShootPhotoView: View {
                 .scaledToFill()
                 .frame(minWidth: 0, maxWidth: .infinity)
                 .edgesIgnoringSafeArea(.all)
+                .onChange(of: image) {
+                    Task {
+                        print("upload an image")
+                        await recognizeBottle()
+                    }
+                }
+            if let probability {
+                Text(target)
+                    .font(.title)
+                Text("bottle: \(probability["bottle"] ?? -1)")
+                Text("not bottle: \(probability["not bottle"] ?? -1)")
+            }
             Button(action: {
                 isShowPhotoLibrary = true
             }, label: {
@@ -33,6 +48,18 @@ struct ShootPhotoView: View {
         }
         .fullScreenCover(isPresented: $isShowPhotoLibrary) {
             ImagePicker(sourceType: .camera, selectedImage: $image)
+                .ignoresSafeArea()
+        }
+    }
+    func recognizeBottle() async {
+        do {
+            let config = MLModelConfiguration()
+            let model = try BottleClassifier1(configuration: config)
+            let prediction = try model.prediction(image: pixelBufferFromImage(image: image))
+            probability = prediction.targetProbability
+            target = prediction.target
+        } catch {
+            print("error")
         }
     }
 }
